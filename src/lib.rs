d40a4d7578548aa41760c89396e9f888c8af1ed3
Trevor Lovell <trevorlovelldesign@gmail.com>
2024-06-22 18:40:30 -0600

    feat: re-export some items in the library

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,8 @@ mod divides;
 mod nom_parsable;
 mod parser_combinators;
 mod rectangle;
+pub use rectangle::HyperRectangle;
 mod sums_in_ratio;
+pub use sums_in_ratio::{Partition, SumsInRatioEvaluationError};
+
+pub type Rpex<const D: usize> = sums_in_ratio::IndeterminateSumsInRatio<D>;
